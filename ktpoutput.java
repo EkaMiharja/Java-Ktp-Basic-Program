16175b68c72e892d93ca2441620e6bb3cfa576e6
M. Eka MIharja <ekamiharja2006@gmail.com>
2025-09-02 17:31:31 +0700

    Add files via upload

---
+++ ktpoutput.java
@@ -0,0 +1,23 @@
+class ktpoutput {
+    public static void main(String[] args) {
+        System.out.println("=====================================");
+        System.out.println("         PROVINSI JAKARTA          ");
+        System.out.println("            KOTA BOGOR     ");
+        System.out.println("=====================================");
+        System.out.println("NIK               : 3201234567890001");
+        System.out.println("Nama              : Eka Miharja");
+        System.out.println("Tempat/Tgl Lahir  : Bogor, 10-08-1995");
+        System.out.println("Jenis Kelamin     : Laki-Laki   Gol. Darah     : O");
+        System.out.println("Alamat            : Jl. Merdeka No. 10");
+        System.out.println("   RT/RW          : 001/002");
+        System.out.println("   Kel/Desa       : Cempaka Baru");
+        System.out.println("   Kecamatan      : Kemayoran");
+        System.out.println("Agama             : Islam");
+        System.out.println("Status Perkawinan : Belum Kawin");
+        System.out.println("Pekerjaan         : Karyawan Swasta");
+        System.out.println("Kewarganegaraan   : WNI");
+        System.out.println("Berlaku Hingga    : Seumur Hidup");
+        System.out.println("=====================================");
+        
+    }
+}
